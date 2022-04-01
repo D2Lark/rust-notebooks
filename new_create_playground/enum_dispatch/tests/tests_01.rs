a72b50619431447862adcaa24d6dd7c1cd183899
d2lark <lichengamoy@gmail.com>
2022-04-01 13:35:39 +0800

    crate enum_dispatch testcast1

---
+++ new_create_playground/enum_dispatch/tests/tests_01.rs
@@ -0,0 +1,39 @@
+use enum_dispatch::enum_dispatch;
+
+#[enum_dispatch(ArrayImpl)]
+trait Array {
+    fn get(&self, index: usize) ;
+}
+
+#[enum_dispatch]
+enum ArrayImpl {
+    StringArray(StringArray),
+    IntArray(IntArray),
+}
+
+pub struct StringArray {
+    data: Vec<u8>,
+}
+pub struct IntArray {
+    data: Vec<i32>,
+}
+
+impl Array for StringArray {
+    fn get(&self, index: usize) {
+        println!("{} index of String array is {}", index, self.data[index]);
+    }
+}
+    
+impl Array for IntArray {
+    fn get(&self, index: usize) {
+        println!("{} index of Int array is {}", index, self.data[index]);
+    }
+}
+    
+#[test]
+fn test_01(){
+    let sa = StringArray{data: "daadddas".as_bytes().to_vec()};
+    let ia = IntArray{data: vec![1,2,3]};
+    sa.get(0);
+    ia.get(0);
+}
