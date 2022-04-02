0b8077d05ab75992566930f39caaa003976f6d27
d2lark <lichengamoy@gmail.com>
2022-04-02 14:53:07 +0800

    optimize stream constructure

--- new_create_playground/futures_async_stream/tests/minitrace.rs
+++ new_create_playground/futures_async_stream/tests/minitrace.rs
@@ -5,59 +5,43 @@ use futures_async_stream::{for_await, stream};
 use minitrace::prelude::*;
 use tokio::pin;
 use tokio::time::{sleep, Duration};
+use futures::stream::{BoxStream};
 #[tokio::test]
 async fn i32_test() {
-    let (root, collector) = Span::root("root");
-    run().in_span(root).await;
-    let records: Vec<SpanRecord> = collector.collect().await;
-    let bytes = minitrace_jaeger::encode("asynchronous".to_owned(), 0, 0, 0, &records).unwrap();
-    minitrace_jaeger::report("127.0.0.1:6831".parse().unwrap(), &bytes)
-        .await
-        .ok();
-}
-async fn run() {
-    let stream = foo(bar(source()));
-    pin!(stream);
-    while let Some(x) = stream.next().await {
-        sleep(Duration::from_secs(1)).await;
+
+    #[for_await]
+    for x in stream3(stream2(stream1())) {
         println!("resutl is {}", x);
     }
+
 }
-#[trace("other job")]
-fn source() -> impl Stream<Item = i32> {
-    #[stream]
-    async move {
-        for i in 0..3 {
-            sleep(Duration::from_secs(1)).await;
-            yield i;
-        }
+pub type BoxedExecutor = BoxStream<'static, i32>;
+
+#[stream(item = i32, boxed)]
+pub async fn stream1() {
+    for i in 1..=3 {
+        println!("stream 1=>init:{}", i);
+        sleep(Duration::from_secs(1)).await;
+        yield i;
     }
 }
 
-#[stream(item = i32)]
-async fn bar(stream: impl Stream<Item = i32>) {
-    // `for_await` is built into `stream`. If you use `for_await` only in `stream`, there is no need to import `for_await`.
-    pin!(stream);
-    while let Some(x) = stream
-        .next()
-        .await
-    {
-        println!("bar span {}", x);
-        sleep(Duration::from_secs(2)).await;
+#[stream(boxed, item = i32)]
+async fn stream2(mut executor_stream: BoxedExecutor){
+    #[for_await]
+    for x in executor_stream {
+        println!("stream 2=>2 + {}", x);
+        sleep(Duration::from_secs(1)).await;
         yield x + 2;
     }
 }
 
-#[stream(item = i32)]
-async fn foo(stream: impl Stream<Item = i32>) {
-    // `for_await` is built into `stream`. If you use `for_await` only in `stream`, there is no need to import `for_await`.
-    pin!(stream);
-    while let Some(x) = stream
-        .next()
-        .await
-    {
-        println!("foo span {}", x);
-        sleep(Duration::from_secs(2)).await;
-        yield x - 2;
+#[stream(boxed, item = i32)]
+async fn stream3(mut executor_stream: BoxedExecutor){
+    #[for_await]
+    for x in executor_stream {
+        println!("stream 3=>3 + {}", x);
+        sleep(Duration::from_secs(1)).await;
+        yield x + 3;
     }
 }
